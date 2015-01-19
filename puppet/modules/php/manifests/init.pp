e11bd2a28b2eaee17f06595a87130150fa2f5797
mdzafar <zafar.asn@gmail.com>
2015-01-19 07:10:44 +0300

    provisioned some more php extensions

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -1,6 +1,6 @@
 class php {
 
-	package { ['php5-fpm',
+	package { ['php5-fpm','php5-gd','php5-curl','php5-mcrypt','php5-mysql',
 						 'php5-cli']:
 	  ensure => present,
 	  require => Exec['apt-get update'],
